f782779d2b3b548629314900ee0cb52c93087638
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-08-07 04:53:52 +0100

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,8 +4,10 @@ export const storeProducts = [
     title: "Watch 1",
     img: require("../images/watch1.jpg"),
     price: 2000,
-    company: "RD",
-    info: "First watch in the goddamn store!!",
+    company: "ARMANI",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
     inCart: false,
     count: 0,
     total: 0,
@@ -15,8 +17,10 @@ export const storeProducts = [
     title: "Watch 2",
     img: require("../images/watch2.jpg"),
     price: 3000,
-    company: "RD",
-    info: "second watch in the goddamn store!!",
+    company: "VERSACE",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
     inCart: false,
     count: 0,
     total: 0,
@@ -26,8 +30,9 @@ export const storeProducts = [
     title: "Watch 3",
     img: require("../images/watch3.jpg"),
     price: 4000,
-    company: "RD",
-    info: "third watch in the goddamn store!!",
+    company: "LOUIS VUITTON",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     inCart: false,
     count: 0,
     total: 0,
@@ -37,8 +42,9 @@ export const storeProducts = [
     title: "Watch 4",
     img: require("../images/watch4.jpg"),
     price: 5000,
-    company: "RD",
-    info: "fourth watch in the goddamn store!!",
+    company: "ROLEX",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     inCart: false,
     count: 0,
     total: 0,
@@ -49,7 +55,8 @@ export const storeProducts = [
     img: require("../images/watch5.jpg"),
     price: 6000,
     company: "RD",
-    info: "fifth watch in the goddamn store!!",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     inCart: false,
     count: 0,
     total: 0,
@@ -60,7 +67,8 @@ export const storeProducts = [
     img: require("../images/watch6.jpg"),
     price: 7000,
     company: "RD",
-    info: "sixth watch in the goddamn store!!",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     inCart: false,
     count: 0,
     total: 0,
@@ -79,3 +87,4 @@ export const detailProduct = {
   count: 0,
   total: 0,
 };
+
