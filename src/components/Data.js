a70fc3d7bb02de44c6f79b4582bfadb7d14fd4a9
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-08-07 04:34:24 +0100

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -73,7 +73,8 @@ export const detailProduct = {
   img: require("../images/watch7.jpg"),
   price: 7000,
   company: "RD",
-  info: "lorem ipsum ksj lakile jbwsjbo;njndb;jw;jnbjckw",
+  info:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   inCart: false,
   count: 0,
   total: 0,
