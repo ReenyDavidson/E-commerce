644e3616ee0c3006fb9a2de851c130d4f62e4c8a
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-08-08 19:29:06 +0100

    Update context.js

--- src/components/contexts/context.js
+++ src/components/contexts/context.js
@@ -4,7 +4,13 @@ import { storeProducts, detailProduct } from "../Data";
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
-  state = { products: [], details: detailProduct };
+  state = {
+    products: [],
+    details: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
+  };
 
   componentDidMount() {
     this.setProducts();
@@ -34,7 +40,27 @@ class ProductProvider extends Component {
   };
 
   addToCart = (id) => {
-    console.log(`add to cart ${id}`);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] };
+    });
+  };
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
   };
 
   render() {
@@ -45,6 +71,8 @@ class ProductProvider extends Component {
             ...this.state,
             handeDetail: this.handleDetail,
             addToCart: this.addToCart,
+            openModal: this.openModal,
+            closeModal: this.closeModal,
           }}
         >
           {this.props.children}
