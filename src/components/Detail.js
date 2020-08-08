8f525b4168d27285d4a54250369a8a2af80e534f
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-08-08 19:34:09 +0100

    Update Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -19,43 +19,41 @@ export default class Detail extends Component {
           } = context.details;
           return (
             <React.Fragment>
-            
-            <div className="card-detail">
-            <h1 className="title">{title}</h1>
-              <div className="product-info">
-                <div className="container1">
-                  <img src={img} alt="product" className="prod-img" />
-                </div>
-                {/*<div className="line"></div>*/}
-                <div className="container2">
+              <div className="card-detail">
+                <h1 className="title">{title}</h1>
+                <div className="product-info">
+                  <div className="container1">
+                    <img src={img} alt="product" className="prod-img" />
+                  </div>
+                  {/*<div className="line"></div>*/}
+                  <div className="container2">
                     <h2 className="prod-author">{company}</h2>
                     <h4 className="prod-price">${price}</h4>
-                 
-                  <div>
-                    <button
-                      className="button1"
-                      disabled={inCart ? true : false}
-                      onClick={() => {
-                        context.addToCart(id);
-                      }}
-                    >
-                      {inCart ? "ADDED TO CART" : "ADD TO CART"}
-                    </button>
-                  </div>
 
+                    <div>
+                      <button
+                        className="button1"
+                        disabled={inCart ? true : false}
+                        onClick={() => {
+                          context.addToCart(id);
+                          context.openModal(id);
+                        }}
+                      >
+                        {inCart ? "IN CART" : "ADD TO CART"}
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="prod-info-container">
-                    <p className="prod-info">
-                      <strong>{info}</strong>
-                    </p>
-                  </div>
-                  
-                    <Link to="/" className="button2">
-                     BACK TO HOME PAGE
-                    </Link>
-                  
+              <div className="prod-info-container">
+                <p className="prod-info">
+                  <strong>{info}</strong>
+                </p>
+              </div>
+
+              <Link to="/" className="button2">
+                BACK TO HOME PAGE
+              </Link>
             </React.Fragment>
           );
         }}
