aa6bd3b978a827dc1c8e11e997ba059ea3e7d3f7
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-08-09 17:43:21 +0100

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -85,6 +85,66 @@ export const storeProducts = [
     count: 0,
     total: 0,
   },
+  {
+    id: 8,
+    title: "Watch 8",
+    img: require("../images/watch8.jpg"),
+    price: 16000,
+    company: "TOM DANIELS",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 9,
+    title: "Watch 9",
+    img: require("../images/watch9.jpg"),
+    price: 19000,
+    company: "LARS WILLIAMS",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 10,
+    title: "Watch 10",
+    img: require("../images/watch10.jpg"),
+    price: 10000,
+    company: "PRADA",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 11,
+    title: "Watch 11",
+    img: require("../images/watch11.jpg"),
+    price: 11000,
+    company: "GUCCI",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 12,
+    title: "Watch 12",
+    img: require("../images/watch12.jpg"),
+    price: 12000,
+    company: "TOM FORD",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
 ];
 
 export const detailProduct = {
