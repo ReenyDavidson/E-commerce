c881df5015e3d4b7ec39067532e1cccb53a61dd4
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-08-09 15:19:02 +0100

    Update Default.js

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 
 export default class Default extends Component {
   render() {
-    return <div>Page not found</div>;
+    console.log();
+    return (
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <div>
+            <h2 style={{ color: "teal" }}>404 error</h2>
+            <h2>Page not found</h2>
+            <h2>
+              The requested page URL{" "}
+              <span style={{ color: "red" }}>
+                {" "}
+                {this.props.location.pathname}{" "}
+              </span>
+              was not found
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
